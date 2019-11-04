9cc2ecee60e07c0c30bd82815aba19407700fa4a
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-11-04 18:57:02 +0100

    Added a database schema

---
+++ db.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 04, 2019 at 06:53 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tester`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `language`
+--
+
+CREATE TABLE `language` (
+  `languageCode` varchar(255) COLLATE utf8mb4_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `translatedword`
+--
+
+CREATE TABLE `translatedword` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `word_id` int(10) UNSIGNED DEFAULT NULL,
+  `translation` varchar(255) COLLATE utf8mb4_polish_ci DEFAULT NULL,
+  `languageCode` varchar(255) COLLATE utf8mb4_polish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_polish_ci DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT NULL,
+  `password_hash` varchar(255) COLLATE utf8mb4_polish_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8mb4_polish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userrole`
+--
+
+CREATE TABLE `userrole` (
+  `role` varchar(255) COLLATE utf8mb4_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userwordstatistics`
+--
+
+CREATE TABLE `userwordstatistics` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `word_id` int(10) UNSIGNED NOT NULL,
+  `word_status` enum('UNKNOWN','LEARNED','MASTERED') COLLATE utf8mb4_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `word`
+--
+
+CREATE TABLE `word` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `created_by` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `language`
+--
+ALTER TABLE `language`
+  ADD PRIMARY KEY (`languageCode`);
+
+--
+-- Indexes for table `translatedword`
+--
+ALTER TABLE `translatedword`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `word_id` (`word_id`),
+  ADD KEY `languageCode` (`languageCode`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role` (`role`);
+
+--
+-- Indexes for table `userrole`
+--
+ALTER TABLE `userrole`
+  ADD PRIMARY KEY (`role`);
+
+--
+-- Indexes for table `userwordstatistics`
+--
+ALTER TABLE `userwordstatistics`
+  ADD PRIMARY KEY (`user_id`,`word_id`),
+  ADD KEY `word_id` (`word_id`);
+
+--
+-- Indexes for table `word`
+--
+ALTER TABLE `word`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `created_by` (`created_by`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `translatedword`
+--
+ALTER TABLE `translatedword`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `word`
+--
+ALTER TABLE `word`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `language`
+--
+ALTER TABLE `language`
+  ADD CONSTRAINT `language_ibfk_1` FOREIGN KEY (`languageCode`) REFERENCES `translatedword` (`languageCode`);
+
+--
+-- Constraints for table `translatedword`
+--
+ALTER TABLE `translatedword`
+  ADD CONSTRAINT `translatedword_ibfk_1` FOREIGN KEY (`word_id`) REFERENCES `word` (`id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role`) REFERENCES `userrole` (`role`);
+
+--
+-- Constraints for table `userwordstatistics`
+--
+ALTER TABLE `userwordstatistics`
+  ADD CONSTRAINT `userwordstatistics_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `word`
+--
+ALTER TABLE `word`
+  ADD CONSTRAINT `word_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
