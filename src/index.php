51d35b31d6d2b192642e868f11373afcf61e6195
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-01-17 20:30:33 +0100

    Hello World on index

--- src/index.php
+++ src/index.php
@@ -1,12 +1,3 @@
 <?php
-    use \Firebase\JWT\JWT;
-    require_once "secrets.php";
-    $header = array(
-        'iss' => 'localhost', // Issuer
-        'aud' => 'localhost', // Audience (who will receive the JWT)
-        'iat' => time(), // Issued At
-        'nbf' => time() + 60 // Not Before
-    );
-    print_r($header);
-    print_r($private_key);
+    echo "Hello, world!";
 ?>
