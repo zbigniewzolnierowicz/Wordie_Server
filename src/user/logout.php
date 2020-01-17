3b5b9863c4460d85f7ddd5fe3a9687ab497b16e4
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-01-17 21:46:27 +0100

    Check if the user was logged in (before logging them out)

--- src/user/logout.php
+++ src/user/logout.php
@@ -2,35 +2,39 @@
 require_once "../includes/is_json.php";
 require_once "../includes/is_cors.php";
 try {
-    // Initialize the session.
-    session_start();
+    if (session_id() != '') { // Check if the session exists
+        // Initialize the session.
+        session_start();
 
-    // Unset all of the session variables.
-    $_SESSION = array();
+        // Unset all of the session variables.
+        $_SESSION = array();
 
-    // If it's desired to kill the session, also delete the session cookie.
-    // Note: This will destroy the session, and not just the session data!
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"]
+        // If it's desired to kill the session, also delete the session cookie.
+        // Note: This will destroy the session, and not just the session data!
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+        $response = array(
+            "response" => "initial",
+            "description" => "Initial"
         );
-    }
-    $response = array(
-        "response" => "initial",
-        "description" => "Initial"
-    );
-    // Finally, destroy the session.
-    session_destroy();
+        // Finally, destroy the session.
+        session_destroy();
 
-    // Check if the session is destroyed
-    if (session_id()) {
-        throw new Exception("session_not_destroyed");
+        // Check if the session is destroyed
+        if (session_id()) {
+            throw new Exception("session_not_destroyed");
+        } else {
+            http_response_code(200);
+            $response['response'] = "log_out_success";
+            $response['description'] = "log_out_success";
+        }
     } else {
-        http_response_code(200);
-        $response['response'] = "log_out_success";
-        $response['description'] = "log_out_success";
+        throw new Exception("not_logged_in");
     }
 } catch (\Throwable $th) {
     http_response_code(500);
