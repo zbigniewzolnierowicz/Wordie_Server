27463931ea72134be308177b0dc513363dd37b7f
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-01-19 16:58:18 +0100

    Add more headers to the CORS include

--- src/includes/is_cors.php
+++ src/includes/is_cors.php
@@ -1,1 +1,5 @@
-<?php header("Access-Control-Allow-Origin: *");?>
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+?>
