c72f7d768e022c5698a334b119f07bd46e8830dd
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-01-19 19:52:44 +0100

    Get user info for an already logged in user

---
+++ src/user/get_user_info.php
@@ -0,0 +1,25 @@
+<?php
+// Include CORS headers
+header('Content-Type: application/json');
+require_once "../includes/is_cors.php";
+require_once "../includes/database_connection.php";
+
+try {
+    $response = [];
+    session_start();
+    // Check if the user is logged in
+    $isUserLoggedInQuery = "SELECT `id`, `name`, `display_name` FROM `user` WHERE `id` = (SELECT `user_id` FROM `sessions` WHERE `session_id` = '" . session_id() . "')";
+    $result = mysqli_query($db, $isUserLoggedInQuery);
+    $data = mysqli_fetch_assoc($result);
+    if ($data) {
+        $response['response'] = "get_user_info_success";
+        $response['user_info'] = $data;
+    }
+} catch (\Throwable $th) {
+    http_response_code($_SERVER['REQUEST_METHOD'] === "OPTIONS" ? 200 : 500);
+    $response['response'] = "get_user_info_fail";
+    $response['description'] = $th->getMessage();
+} finally {
+    echo json_encode($response);
+}
+?>
