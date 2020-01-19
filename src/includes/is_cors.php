1fb67c72c740659aa3d82ec34c86b28fb2fab1a2
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-01-19 19:53:18 +0100

    Change some CORS headers

--- src/includes/is_cors.php
+++ src/includes/is_cors.php
@@ -1,5 +1,6 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: http://localhost:8080");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+    header("Access-Control-Allow-Credentials: true")
 ?>
